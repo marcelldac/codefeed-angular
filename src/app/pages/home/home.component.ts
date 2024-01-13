83775c3bd9e3c5229ce2ec7425ceb85859b45cfb
marcelldac <dacmarcell@gmail.com>
2024-01-13 16:29:01 -0300

    feat: app quiz component

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { QuizComponent } from '../../components/quiz/quiz.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [QuizComponent],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
-export class HomeComponent {
-
-}
+export class HomeComponent {}
